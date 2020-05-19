89e20cd9a526ac4a478c1f1ccc90d1c9bb357d7a
Alexander <dyndinalexandr13@gmail.com>
2020-05-19 17:23:21 +0300

    initial2

--- app/src/main/java/com/alexandr/deadlineapp/Presentation/Acrivities/Main/DeadlineFragment.kt
+++ app/src/main/java/com/alexandr/deadlineapp/Presentation/Acrivities/Main/DeadlineFragment.kt
@@ -1,61 +1,53 @@
 package com.alexandr.deadlineapp.Presentation.Acrivities.Main
 
-import android.app.Application
-import android.app.DatePickerDialog
-import android.app.TimePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.alexandr.deadlineapp.App
 import com.alexandr.deadlineapp.Domain.DeadlineViewModel
 import com.alexandr.deadlineapp.Presentation.Adapter.MyDeadlineRecyclerViewAdapter
 import com.alexandr.deadlineapp.R
 import com.alexandr.deadlineapp.Repository.Database.Entity.Deadline
-import com.alexandr.deadlineapp.Utils.Utils
-import com.alexandr.deadlineapp.di.component.ViewModelComponent
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.bottom_sheet.*
 import kotlinx.android.synthetic.main.deadline_fragment.*
-import java.util.*
-import javax.inject.Inject
-import kotlin.collections.ArrayList
 
 
-class DeadlineFragment : Fragment() {
+class DeadlineFragment : Fragment(), View.OnClickListener {
+
+    private lateinit var mainActivity: MainActivity
+    private lateinit var viewModel: DeadlineViewModel
 
     companion object {
         fun newInstance() =
             DeadlineFragment()
     }
 
-
-    private var viewModel1: DeadlineViewModel? = null
-        @Inject set
-
-    private lateinit var viewModel: DeadlineViewModel
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-
         return inflater.inflate(R.layout.deadline_fragment, container, false)
     }
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        createDaggerDependencies()
         viewModel = ViewModelProviders.of(this).get(DeadlineViewModel::class.java)
+        mainActivity = activity as MainActivity
+        requestData(viewModel)
+        setRecycler(recycler)
+        mainActivity.btadd.setOnClickListener(this)
+    }
+
+
+    private fun requestData(viewModel: DeadlineViewModel) {
         viewModel.requestInformation()
         recycler.layoutManager = LinearLayoutManager(context)
         viewModel.deadlines.observe(viewLifecycleOwner, Observer<List<Deadline>>{
@@ -66,99 +58,42 @@ class DeadlineFragment : Fragment() {
                     viewModel
                 )
         })
-        registerForContextMenu(recycler);
+    }
 
+    private fun setRecycler(recycler: RecyclerView) {
+        registerForContextMenu(recycler);
         recycler.addOnScrollListener(object : RecyclerView.OnScrollListener() {
             override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
                 super.onScrolled(recyclerView, dx, dy)
                 if (dy >0) {
-                    (activity as MainActivity?)?.bottomHide()
+                    mainActivity.bottomHide()
+                    mainActivity.fab.hide()
                 }
                 else if (dy <0) {
-                    (activity as MainActivity?)?.bottomHide()
+                    mainActivity.bottomHide()
+                    mainActivity.fab.show()
                 }
             }
         })
-        activity?.imgPinned?.setOnClickListener {
-            if (activity?.imgPinned?.contentDescription == "pinned"){
-                activity?.imgPinned?.setImageResource(R.drawable.ic_unpin)
-                activity?.imgPinned?.contentDescription = "unpinned"
-            } else {
-                activity?.imgPinned?.setImageResource(R.drawable.ic_pin)
-                activity?.imgPinned?.contentDescription = "pinned"
-            }
-            true
-        }
-        activity?.btadd?.setOnClickListener {
-            val predmet =  activity?.editPredmet?.text.toString()
-            val descr = activity?.editDescription?.text.toString()
-            var color: Int = R.color.colorLow;
-            if (activity?.chipMedium?.isChecked?: false){
-                color = R.color.colorMedium
-            }
-            if (activity?.chipHigh?.isChecked?: false){
-                color = R.color.colorHigh
-            }
-            val pinned = activity?.imgPinned?.contentDescription == "pinned"
-            val date = activity?.editDate?.text.toString()
-            val time = activity?.editTime?.text.toString()
-            val d = Deadline(name = predmet, description = descr, pinned = pinned,
-                date = date, time = time, importance = color)
-            viewModel?.saveInformation(d)
-            (activity as MainActivity?)?.clear()
-            Toast.makeText(requireContext(), "Дедлайн добавлен!", Toast.LENGTH_SHORT).show()
-            (activity as MainActivity?)?.bottomHide()
-        }
-        activity?.imgCal?.setOnClickListener {
-            DatePickerDialog(requireContext(),
-                DatePickerDialog.OnDateSetListener { _, year, monthOfYear, dayOfMonth ->
-                val c = Calendar.getInstance()
-                c.set(year,monthOfYear,dayOfMonth)
-                var date: String = "${Utils.WeekDay(c.get(Calendar.DAY_OF_WEEK))}, ${year}."
-                    date += if (monthOfYear<10){
-                        "0${monthOfYear}."
-                    } else {
-                        "${monthOfYear}."
-                    }
-                    date += if (dayOfMonth<10){
-                        "0${dayOfMonth}"
-                    } else {
-                        "$dayOfMonth"
-                    }
-                    activity?.editDate?.setText(date)
-            }, Calendar.getInstance().get(Calendar.YEAR),
-                Calendar.getInstance().get(Calendar.MONTH),
-                Calendar.getInstance().get(Calendar.DAY_OF_MONTH)).show()
-        }
-        activity?.imgClk?.setOnClickListener {
-            TimePickerDialog(requireContext(),
-                TimePickerDialog.OnTimeSetListener { _, hourOfDay, minute ->
-                var time: String = if (hourOfDay<10){
-                        "0${hourOfDay}:"
-                    } else {
-                        "${hourOfDay}:"
-                    }
-                    time += if (minute<10){
-                        "0${minute}"
-                    } else {
-                        "$minute"
-                    }
-                    activity?.editTime?.setText(time)
-            }, Calendar.getInstance().get(Calendar.HOUR_OF_DAY),
-                Calendar.getInstance().get(Calendar.MINUTE),true).show()
-        }
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onClick(v: View?) {
+        val predmet =  mainActivity.editPredmet.text.toString()
+        val descr = mainActivity.editDescription.text.toString()
+        var color: Int = R.color.colorLow;
+        if (mainActivity.chipMedium.isChecked){
+            color = R.color.colorMedium
+        }
+        if (mainActivity.chipHigh.isChecked){
+            color = R.color.colorHigh
+        }
+        val pinned =mainActivity.imgPinned.contentDescription == "pinned"
+        val date = mainActivity.editDate.text.toString()
+        val time = mainActivity.editTime.text.toString()
+        val d = Deadline(name = predmet, description = descr, pinned = pinned,
+            date = date, time = time, importance = color)
+        viewModel.saveInformation(d)
+        Toast.makeText(requireContext(), "Дедлайн добавлен!", Toast.LENGTH_SHORT).show()
+        mainActivity.bottomHide()
     }
-
-    private fun createDaggerDependencies() {
-        injectDependency((activity?.application as App).getViewModelComponent())
-    }
-
-    private fun injectDependency(component: ViewModelComponent) {
-        component.inject(this)
-    }
-
 }
--- app/src/main/java/com/alexandr/deadlineapp/Presentation/Acrivities/Main/MainActivity.kt
+++ app/src/main/java/com/alexandr/deadlineapp/Presentation/Acrivities/Main/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.alexandr.deadlineapp.Presentation.Acrivities.Main
 
-import android.app.Activity
-import android.app.AlarmManager
-import android.app.PendingIntent
+import android.app.*
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
@@ -11,42 +9,60 @@ import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.alexandr.deadlineapp.Presentation.Item.TimeNotification
 import com.alexandr.deadlineapp.R
+import com.alexandr.deadlineapp.Utils.Utils
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.bottom_sheet.*
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<LinearLayout>
     private var touchtoback: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setToolbar(resources.getString(R.string.title))
+        fab.setOnClickListener(this)
+        bottomSheetBehavior = BottomSheetBehavior.from(bottom_sheet_layout)
+        setBottom(bottomSheetBehavior)
+    }
+
+    private fun setToolbar(title: String){
         setSupportActionBar(toolbar)
-        supportActionBar?.title = "Дедлайны"
-        var bottomSheetBehavior = BottomSheetBehavior.from(bottom_sheet_layout)
-        fab.setOnClickListener {
-            bottomShow()
-        }
+        supportActionBar?.title = title
+    }
+
+    fun bottomShow(){
+        if (bottomSheetBehavior.state != BottomSheetBehavior.STATE_EXPANDED)
+        {bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED}
+    }
+
+    fun bottomHide(){
+        if (bottomSheetBehavior.state != BottomSheetBehavior.STATE_COLLAPSED)
+        {bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED}
+    }
+
+    private fun setBottom(bottomSheetBehavior: BottomSheetBehavior<LinearLayout>){
         bottomSheetBehavior.addBottomSheetCallback(object :
             BottomSheetBehavior.BottomSheetCallback() {
             override fun onStateChanged(bottomSheet: View, newState: Int) {
                 when (newState) {
                     BottomSheetBehavior.STATE_HIDDEN -> {
-                        val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
-                        inputMethodManager.hideSoftInputFromWindow(bottom_sheet_layout.windowToken, 0)
+
                     }
                     BottomSheetBehavior.STATE_EXPANDED -> {
 
                     }
                     BottomSheetBehavior.STATE_COLLAPSED -> {
                         clear()
-                        val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
-                        inputMethodManager.hideSoftInputFromWindow(bottom_sheet_layout.windowToken, 0)
                     }
                     BottomSheetBehavior.STATE_DRAGGING -> {
 
@@ -55,7 +71,6 @@ class MainActivity : AppCompatActivity() {
                     }
                 }
             }
-
             override fun onSlide(
                 bottomSheet: View,
                 slideOffset: Float
@@ -63,16 +78,48 @@ class MainActivity : AppCompatActivity() {
                 fab.animate().scaleX(1 - slideOffset).scaleY(1 - slideOffset).setDuration(0).start();
             }
         })
-    }
 
-    fun bottomShow(){
-        var bottomSheetBehavior = BottomSheetBehavior.from(bottom_sheet_layout)
-        bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
-    }
+        imgCal.setOnClickListener {
+            DatePickerDialog(this,
+                DatePickerDialog.OnDateSetListener { _, year, monthOfYear, dayOfMonth ->
+                    val c = Calendar.getInstance()
+                    c.set(year,monthOfYear,dayOfMonth)
+                    var date: String = "${Utils.WeekDay(c.get(Calendar.DAY_OF_WEEK))}, ${year}."
+                    date += if (monthOfYear<10){
+                        "0${monthOfYear}."
+                    } else { "${monthOfYear}." }
+                    date += if (dayOfMonth<10){
+                        "0${dayOfMonth}"
+                    } else { "$dayOfMonth"   }
+                    editDate.setText(date)
+                }, Calendar.getInstance().get(Calendar.YEAR),
+                Calendar.getInstance().get(Calendar.MONTH),
+                Calendar.getInstance().get(Calendar.DAY_OF_MONTH)).show()
+        }
 
-    fun bottomHide(){
-        var bottomSheetBehavior = BottomSheetBehavior.from(bottom_sheet_layout)
-        bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+        imgClk.setOnClickListener {
+            TimePickerDialog(this,
+                TimePickerDialog.OnTimeSetListener { _, hourOfDay, minute ->
+                    var time: String = if (hourOfDay<10){
+                        "0${hourOfDay}:"
+                    } else { "${hourOfDay}:" }
+                    time += if (minute<10){
+                        "0${minute}"
+                    } else { "$minute" }
+                    editTime.setText(time)
+                }, Calendar.getInstance().get(Calendar.HOUR_OF_DAY),
+                Calendar.getInstance().get(Calendar.MINUTE),true).show()
+        }
+
+        imgPinned.setOnClickListener { it as ImageView
+            if (it.contentDescription == "pinned"){
+                it.setImageResource(R.drawable.ic_unpin)
+                it.contentDescription = "unpinned"
+            } else {
+                it.setImageResource(R.drawable.ic_pin)
+                it.contentDescription = "pinned"
+            }
+        }
     }
 
     fun clear(){
@@ -80,7 +127,10 @@ class MainActivity : AppCompatActivity() {
         editTime.text.clear()
         editDescription.text.clear()
         editPredmet.text.clear()
+        val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(bottom_sheet_layout.windowToken, 0)
     }
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
@@ -95,6 +145,7 @@ class MainActivity : AppCompatActivity() {
             else -> super.onOptionsItemSelected(item)
         }
     }
+
     fun restartNotify(){
         val am = getSystemService(Context.ALARM_SERVICE) as AlarmManager
         val intent = Intent(this,TimeNotification::class.java)
@@ -115,9 +166,13 @@ class MainActivity : AppCompatActivity() {
             return;
         }
         if (touchtoback == 1) {
-                Toast.makeText(this, "Нажмите НАЗАД еще раз для выхода", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, resources.getString(R.string.pressBack), Toast.LENGTH_SHORT).show()
                 Handler().postDelayed(Runnable { touchtoback-- }, 2000)
         } else { super.onBackPressed(); }
+    }
+
+    override fun onClick(v: View?) {
+        bottomShow()
     }
 
 }
